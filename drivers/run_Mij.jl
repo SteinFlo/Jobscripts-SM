c1e59b056a3e6874c9abbdad1e8c4a98ca580c2f
SteinFlo <flst00003@stud.uni-saarland.de>
2026-02-13 16:16:22 +0100

    driver to get Fij part of Mij

--- drivers/run_Mij.jl
+++ drivers/run_Mij.jl
@@ -1,30 +1,68 @@
+#!/opt/julia-1.12.3/bin/julia
+include("/nethome/fstein/repositories/schwingermodeltn/src/timeEvolution/Multiproduct.jl")
 
-dt0 = 0.01
+if length(ARGS) == 13
 
-dts = [0.03 0.04 0.05 0.06]
+    N         = parse(Int64,   ARGS[1])
+    dimension = parse(Int64,   ARGS[2])
+    trunc     = ARGS[3]
+    x         = parse(Float64, ARGS[4])
 
+    mg        = parse(Float64, ARGS[5])
+    l0        = parse(Float64, ARGS[6])
+    epsilon   = 1 
 
-N 
-d
-trunc
-x
-mg
-l0
-t
-order 
-cutoff
-
-k0 = ceil(t/dt0)
-ks = ceil.(t./dts)
-
-numk = length(ks)
-if l <= numk
-    get_Fij_MPO(t, k0, k[l], s, x, mg, l0, trunc, dirname, cutoff=cutoff, order=order)
-else
-    lp = l - numk -1
-    i = div(lp, numk)+1
-    j = mod(lp, numk)+1
-    if i!=j
-        get_Fij_MPO(t, k[i], k[j], s, x, mg, l0, trunc, dirname, cutoff=cutoff, order=order)
+    dt0       = parse(Float64, ARGS[7])
+    dts = let expr = Meta.parse(ARGS[8])
+        @assert expr.head == :vect
+        Float64.(expr.args)
     end
+    timestep = parse(Float64, ARGS[9])
+
+    order     = parse(Int64,   ARGS[10])
+    cutoff    = parse(Float64, ARGS[11])
+    basedir   = ARGS[12]
+    counter   = parse(Int64,   ARGS[13])
+
+    numk = length(dts)
+
+
+    counter2 = mod(counter-1, numk^2+numk)+1
+    counter_t = div(counter, numk^2+numk)+1
+
+    t = timestep * counter_t
+    k0 = Int(round(t/dt0))
+    ks = Int.(round.(t./dts))
+
+    dirname = string(basedir, "/MPF/")
+    truncstr = trunc[1]
+    if truncstr == 'b'
+        truncstr=string()
+    end
+
+    s = getSites(N, dimension)
+
+    if counter_t <= numk
+        outputname = string(dirname, "out/Fij/", "N", N, "_d", dimension, truncstr, "_x", x, "_mg", mg, "_l0", l0, "_t", t, "_ki", k0, "_kj", ks[counter2], "_order", order, "_cutoff", cutoff, ".out")
+        open(outputname, "w") do file
+            redirect_stdout(file) do
+                get_Fij_MPO(t, k0, ks[counter2], s, x, mg, l0, trunc, dirname, cutoff=cutoff, order=order)
+            end
+        end
+    else
+        counter_p = counter2 - numk -1
+        i = div(counter_p, numk)+1
+        j = mod(counter_p, numk)+1
+        if i!=j
+            outputname = string(dirname, "out/Fij/", "N", N, "_d", dimension, truncstr, "_x", x, "_mg", mg, "_l0", l0, "_t", t, "_ki", ks[i], "_kj", ks[j], "_order", order, "_cutoff", cutoff, ".out")
+            open(outputname, "w") do file
+                redirect_stdout(file) do
+                    get_Fij_MPO(t, ks[i], ks[j], s, x, mg, l0, trunc, dirname, cutoff=cutoff, order=order)
+                end
+            end
+        end
+    end
+else
+    println("Wrong number of arguments. Usage:")
+    println("run_Mij.jl N d truncation x mg l0 dt0 [dts] timestep order cutoff basedir counter")
 end
